04bd9814f208417cb7bf5fe3f0d6ce4eeb492e8c
Jhoan Saldarriaga <ale.xo64@hotmail.com>
2023-02-12 11:20:33 -0500

    Ultimo commit para iniciar con el proceso en otras ramas.

--- API-COVID19/Controllers/UpdateFileController.cs
+++ API-COVID19/Controllers/UpdateFileController.cs
@@ -7,14 +7,33 @@ namespace API_COVID19.Controllers
     public class UpdateFileController : Controller
     {
         [HttpGet]
-        [Route("UpdateFile")]
-        public async Task<IActionResult> GetFile() 
+        [Route("GetLastDailyReportFile")]
+        public async Task<IActionResult> GetLastDailyReportFile() 
         {
             using(var client  = new HttpClient() )
             {
                 var urlFile = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/01-11-2023.csv";
 
                 using (var response = await client.GetAsync(urlFile) )
+                {
+                    response.EnsureSuccessStatusCode();
+                    var fileContent = await response.Content.ReadAsStringAsync();
+
+                }
+
+                return Ok("File downloaded succesfully");
+            }
+        }
+
+        [HttpGet]
+        [Route("GetDataCountries")]
+        public async Task<IActionResult> GetFile()
+        {
+            using (var client = new HttpClient())
+            {
+                var urlFile = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv";
+
+                using (var response = await client.GetAsync(urlFile))
                 {
                     response.EnsureSuccessStatusCode();
                     var fileContent = await response.Content.ReadAsStringAsync();
