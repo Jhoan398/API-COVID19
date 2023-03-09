214dd3f2eacd5317bca3cf441978ced099bb379d
Jhoan Saldarriaga <ale.xo64@hotmail.com>
2023-03-08 19:55:03 -0500

    Respectivas migraciones

---
+++ API-COVID19/Migrations/20230309005344_Fix V.1.1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICOVID19.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixV11 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateReport",
+                table: "Cases",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Recovered",
+                table: "Cases",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateReport",
+                table: "Cases");
+
+            migrationBuilder.DropColumn(
+                name: "Recovered",
+                table: "Cases");
+        }
+    }
+}
