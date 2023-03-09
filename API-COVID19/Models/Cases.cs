103b7a2cdf4319daba6b192e6bcd125941bf178c
Jhoan Saldarriaga <ale.xo64@hotmail.com>
2023-03-08 19:54:37 -0500

    Fix Cases

--- API-COVID19/Models/Cases.cs
+++ API-COVID19/Models/Cases.cs
@@ -11,7 +11,11 @@ namespace API_COVID19.Models
         public int Id { get; set; }
         public decimal Deaths { get; set; }
         public decimal Confirmed { get; set; }
+        public decimal Recovered { get; set; }
         public int CountryId { get; set; }
+        public int? ProvinceStateId { get; set; }
+
+        public DateTime DateReport { get; set; }    
 
         [ForeignKey("CountryId")]
         public virtual Country CountryCaseReport { get; set; }
