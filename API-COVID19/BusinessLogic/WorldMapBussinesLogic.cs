07274225979310aa9389d4bc6892e39410319e52
Jhoan Saldarriaga <ale.xo64@hotmail.com>
2023-03-17 21:29:03 -0500

    Controlador para WorldMapData

---
+++ API-COVID19/BusinessLogic/WorldMapBussinesLogic.cs
@@ -0,0 +1,21 @@
+﻿using API_COVID19.Models;
+
+namespace API_COVID19.BusinessLogic
+{
+    public class WorldMapBussinesLogic
+    {
+
+        private ApplicationDbContext _dbContext;
+
+        public WorldMapBussinesLogic(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public WorldmapData GetWorldMapData() 
+        {
+            return _dbContext.WorldmapData.FirstOrDefault();
+        }
+    }
+}
---
+++ API-COVID19/Controllers/WorldMapController.cs
@@ -0,0 +1,39 @@
+﻿using API_COVID19.BusinessLogic;
+using API_COVID19.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_COVID19.Controllers
+{
+    [ApiController]
+    [Route("WorldMap")]
+    public class WorldMapController : Controller
+    {
+        private readonly WorldMapBussinesLogic _db;
+
+        public WorldMapController(ApplicationDbContext AppDataContext)
+        {
+            _db = new WorldMapBussinesLogic(AppDataContext);
+        }
+
+        [HttpGet]
+        [Route("WorldMapData")]
+        public async Task<IActionResult> WorldMapData()
+        {
+
+            try
+            {
+                var WorldMapData = _db.GetWorldMapData();
+
+                return Ok(WorldMapData);
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+
+        }
+
+
+    }
+}
