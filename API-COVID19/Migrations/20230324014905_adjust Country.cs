fcd344a26b785b988297a0f28734d11800b108cf
Jhoan Saldarriaga <ale.xo64@hotmail.com>
2023-03-25 00:17:05 -0500

    Ajuste para exponer el ISO3 de los paises

---
+++ API-COVID19/Migrations/20230324014905_adjust Country.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICOVID19.Migrations
+{
+    /// <inheritdoc />
+    public partial class adjustCountry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ISO3",
+                table: "Country",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ISO3",
+                table: "Country");
+        }
+    }
+}
